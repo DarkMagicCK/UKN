f4cb3cdfd1d363a65265520ca884257944c1434e
darkfall <darkfall@gamemastercn.com>
2011-11-19 01:25:11 -0600

    Initial Commit

---
+++ Project Unknown/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project Unknown
+//
+//  Created by Ruiwei Bu on 11/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

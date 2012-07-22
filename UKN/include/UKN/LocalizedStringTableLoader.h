489f0e97bad3e12dc814d68e9356121d434e2b8b
Robert Bu <darkfall3@gmail.com>
2012-07-22 14:05:22 +0800

    adjust LocalizedStringTable

---
+++ UKN/include/UKN/LocalizedStringTableLoader.h
@@ -0,0 +1,14 @@
+//
+//  StringTableLoader.h
+//  Project Unknown
+//
+//  Created by Robert Bu on 7/22/12.
+//  Copyright (c) 2012 heizi. All rights reserved.
+//
+
+#ifndef Project_Unknown_StringTableLoader_h
+#define Project_Unknown_StringTableLoader_h
+
+
+
+#endif
---
+++ UKN/src/Core/LocalizedStringTableLoader.cpp
@@ -0,0 +1,9 @@
+//
+//  LocalizedStringTableLoader.cpp
+//  Project Unknown
+//
+//  Created by Robert Bu on 7/22/12.
+//  Copyright (c) 2012 heizi. All rights reserved.
+//
+
+#include "LocalizedStringTableLoader.h"

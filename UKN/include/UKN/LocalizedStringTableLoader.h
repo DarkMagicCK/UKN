9bafe4212b12004f7df69482f193fc41b93c0802
Robert Bu <darkfall3@gmail.com>
2012-07-25 16:13:20 +0800

    adjust LocalizedStringtable

--- UKN/include/UKN/LocalizedStringTableLoader.h
+++ UKN/include/UKN/LocalizedStringTableLoader.h
@@ -9,6 +9,15 @@
 #ifndef Project_Unknown_StringTableLoader_h
 #define Project_Unknown_StringTableLoader_h
 
-
+namespace ukn {
+    
+    class LocalizedStringTable;
+    
+    struct LocalizedStringTableLoader {
+        static bool Load(const char* file, LocalizedStringTable& stringTable);
+        static bool Save(const char* file, const LocalizedStringTable& stringTable);
+    };
+    
+}
 
 #endif

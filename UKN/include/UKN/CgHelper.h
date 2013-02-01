6618dbcf7df541b17f01983d6e87d75351ea28d6
Robert Bu <darkfall@gamemastercn.com>
2013-02-01 12:31:28 -0800

    add begin/end2dRendering for GLPlugin; add device feature level detection in CgDxShader

---
+++ UKN/include/UKN/CgHelper.h
@@ -0,0 +1,18 @@
+#ifndef UKN_CG_HELPER_H_
+#define UKN_CG_HELPER_H_
+
+
+#include "UKN/PreDeclare.h"
+
+namespace ukn {
+
+    /* creating effects some simple cg shaders shared by different renderers */
+    /* Shader Provider must be Cg */
+
+    UKN_API EffectPtr CreateCgEffet2D();    
+
+
+} // namespace ukn
+
+
+#endif
---
+++ UKN/src/Render/CgHelper.cpp
@@ -0,0 +1,59 @@
+#include "UKN/CgHelper.h"
+#include "UKN/GraphicFactory.h"
+#include "UKN/Shader.h"
+#include "UKN/Context.h"
+
+#include "mist/Logger.h"
+
+namespace ukn {
+
+    namespace {
+        static const char* _2d_vert_program = "const uniform float4x4 viewMatrix;\
+                                              const uniform float4x4 projectionMatrix;\
+                                              const uniform float4x4 worldMatrix;\
+                                              struct VertexOutputType {\
+                                              float4 position: POSITION;\
+                                              float2 texCoord: TEXCOORD0;\
+                                              float4 color: COLOR;\
+                                              };\
+                                              VertexOutputType VertexProgram(in float2 texCoord: TEXCOORD0,\
+                                              in float3 position: POSITION,\
+                                              in float4 color: COLOR) {\
+                                              VertexOutputType output;\
+                                              output.position = float4(position, 1);\
+                                              output.position = mul(output.position, viewMatrix);\
+                                              output.position = mul(output.position, projectionMatrix);\
+                                              output.texCoord = texCoord;\
+                                              output.color = color;\
+                                              return output;\
+                                              }\0";
+
+        static const char* _2d_frag_program = "struct VertexOutputType {\
+                                              float4 position: POSITION;\
+                                              float2 texCoord: TEXCOORD0;\
+                                              float4 color: COLOR;\
+                                              };\
+                                              float4 FragmentProgram(VertexOutputType input,\
+                                              uniform sampler2D tex: TEX): COLOR {\
+                                              float4 texColor = tex2D(tex, input.texCoord);\
+                                              return texColor;\
+                                              }\0";
+
+    }
+
+    EffectPtr CreateCgEffet2D() {
+        EffectPtr effect = ukn::Context::Instance().getGraphicFactory().createEffect();
+        effect->setFragmentShader(effect->createShader(ukn::Resource::MakeResourcePtr(new ukn::MemoryStream((const uint8*)_2d_frag_program, 
+            strlen(_2d_frag_program)), L""),
+            ukn::ShaderDesc(ST_FragmentShader,
+            "FragmentProgram")));
+        effect->setVertexShader(effect->createShader(ukn::Resource::MakeResourcePtr(new ukn::MemoryStream((const uint8*)_2d_vert_program, 
+            strlen(_2d_vert_program)), L""),
+            ukn::ShaderDesc(ST_VertexShader,
+            "VertexProgram")));
+        effect->setVertexFormat(ukn::Vertex2D::Format());
+        return effect;
+    }
+
+
+}
